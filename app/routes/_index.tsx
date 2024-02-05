f17899874514589df4a691bf3524f0323bb5f6b8
= <=>
2024-02-04 16:25:41 -0800

    Added tailwind and shadcn

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@components/ui/button";
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
@@ -10,7 +11,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <Button>Click Me</Button>
       <ul>
         <li>
           <a
